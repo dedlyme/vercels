91ac3d05729994bf63aa67eb2962065ec82620d2
dedlyme <k.vasarajs@gmail.com>
2025-02-20 08:48:04 +0200

    done
    and dusted

---
+++ marssels_ara/src/App.js
@@ -0,0 +1,65 @@
+
+import './App.css';
+
+function App() {
+  return (
+  <div className="App">
+      <table>
+        <thead>
+          <tr>
+              <th>english</th>
+              <th>latviski</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+              <td>feature</td>
+              <td>Funkcionalitāte</td>
+          </tr>
+          <tr>
+              <td>Background</td>
+              <td>Konteksts</td>
+          </tr>
+          <tr>
+              <td>rule</td>
+              <td>noteikums</td>
+          </tr>
+          <tr>
+              <td>Scenario</td>
+              <td>Scenārijs</td>
+          </tr>
+          <tr>
+              <td>Scenario Outline</td>
+              <td>Scenārijs pēc parauga</td>
+          </tr>
+          <tr>
+              <td>Examples</td>
+              <td>piemērs</td>
+          </tr>
+          <tr>
+              <td>Given</td>
+              <td>Kad</td>
+          </tr>
+          <tr>
+              <td>When</td>
+              <td>Ja</td>
+          </tr>
+          <tr>
+              <td>Then</td>
+              <td>Tad</td>
+          </tr>
+          <tr>
+              <td>And</td>
+              <td>Un</td>
+          </tr>
+          <tr>
+              <td>But</td>
+              <td>Bet</td>
+          </tr>
+          </tbody>
+      </table>
+  </div>
+  );
+  }
+
+export default App;
